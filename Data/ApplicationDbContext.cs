506aa6257bd3e7734539930d565d80f0ccd5d649
Горбулёв Сергей <gorbulevsv@yandex.ru>
2020-10-13 20:30:06 +0300

    Добавьте файлы проекта.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using blazorControlPanel.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace blazorControlPanel.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<user>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+            posts = Set<post>();
+            comments = Set<comment>();
+            likes = Set<like>();
+            imageAlbums = Set<imageAlbum>();
+        }
+
+        public DbSet<post> posts { get; set; }
+        public DbSet<comment> comments { get; set; }
+        public DbSet<like> likes { get; set; }
+        public DbSet<imageAlbum> imageAlbums { get; set; }
+    }
+}
---
+++ Services/IPostsServise.cs
@@ -0,0 +1,28 @@
+﻿using blazorControlPanel.Data;
+using blazorControlPanel.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazorControlPanel.Services
+{
+    interface IPostsServise
+    {
+        Task<List<post>> posts();
+    }
+
+    public class PostsService : IPostsServise
+    {
+        ApplicationDbContext _context;
+        public PostsService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<post>> posts()
+        {
+            return await _context.posts.ToListAsync();         
+        }
+    }
+}
