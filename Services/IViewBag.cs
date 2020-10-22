0785dea2b62b949e4b5cfc90466d62eda8800636
Горбулёв Сергей <gorbulevsv@yandex.ru>
2020-10-22 23:20:31 +0300

    1

--- Services/IViewBag.cs
+++ Services/IViewBag.cs
@@ -8,26 +8,20 @@ namespace blazorControlPanel.Services
     public interface IViewBag
     {
         string заголовок_страницы { get; set; }
-        event Action Заголовок_обновлён;
-        void Обновить_заголовок();
+        event Action Заголовок_изменён;
     }
 
     public class ViewBag : IViewBag
     {
         private string _Заголовок_страницы;
+        public event Action Заголовок_изменён;
         public string заголовок_страницы 
         {
             get { return _Заголовок_страницы; }
             set {
                 _Заголовок_страницы = value;
+                Заголовок_изменён?.Invoke();
             }
-        }
-
-        public event Action Заголовок_обновлён;
-
-        public void Обновить_заголовок()
-        {
-            Заголовок_обновлён?.Invoke();
         }
     }
 }
