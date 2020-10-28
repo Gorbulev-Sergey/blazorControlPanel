bebb619ad7fbb931dd9cfcbcaecea450edcfe553
Горбулёв Сергей <gorbulevsv@yandex.ru>
2020-10-28 21:47:33 +0300

    1

--- Services/ISchedulesService.cs
+++ Services/ISchedulesService.cs
@@ -11,8 +11,7 @@ namespace blazorControlPanel.Services
 {
     interface ISchedulesService
     {
-        List<schedule_string> schedule_not_async(DateTime schedule_year_and_month);
-        Task<List<schedule_string>> schedule(DateTime schedule_year_and_month);
+        List<schedule_string> schedule(DateTime schedule_year_and_month);
         Task create(List<schedule_string> schedule);
         Task update(List<schedule_string> schedule);
         Task delete(List<schedule_string> schedule);
@@ -25,14 +24,9 @@ namespace blazorControlPanel.Services
         {
             _context = context;
         }
-        public List<schedule_string> schedule_not_async(DateTime schedule_year_and_month) 
+        public List<schedule_string> schedule(DateTime schedule_year_and_month) 
         { 
             return _context.schedule.Where(s => s.date_and_time.Year == schedule_year_and_month.Year && s.date_and_time.Month == schedule_year_and_month.Month).ToList();
-
-        }
-        public async Task<List<schedule_string>> schedule(DateTime schedule_year_and_month)
-        {
-            return await _context.schedule.Where(s => s.date_and_time.Year == schedule_year_and_month.Year && s.date_and_time.Month == schedule_year_and_month.Month).ToListAsync();
         }
         public async Task create(List<schedule_string> schedule)
         {
@@ -47,8 +41,21 @@ namespace blazorControlPanel.Services
         }
         public async Task update(List<schedule_string> schedule)
         {
-            //_context.Update<schedule>(schedule);
-            //await _context.SaveChangesAsync();
+            foreach (var str in schedule)
+            {
+                if (!String.IsNullOrEmpty(str.description) || str.prayer != тип_службы.пусто || str.date_and_time.ToString() == "0:00")
+                {                    
+                    try
+                    {
+                        _context.Update(str);                        
+                    }
+                    catch
+                    {
+                        _context.schedule.Add(str);
+                    }
+                }
+            }
+            await _context.SaveChangesAsync();
         }
         public async Task delete(List<schedule_string> schedule)
         {
