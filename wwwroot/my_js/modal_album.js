104372f91b3fe6068d879bf7452a75d119129c19
Серёжа <gorbulevsv@yandex.ru>
2020-11-06 21:50:37 +0300

    10

---
+++ wwwroot/my_js/modal_album.js
@@ -0,0 +1,13 @@
+﻿var Show_album = function (modal) {
+    document.getElementById(modal).requestFullscreen();
+    //$("#"+ modal).modal('show');
+};
+var Hidden_album = function (modal) {
+    document.exitFullscreen();
+    //$("#" + modal).modal('hide');
+};
+$(document).on('fullscreenchange', function () {
+    if (!document.fullscreenElement) {
+        Hidden_album();
+    }
+});
